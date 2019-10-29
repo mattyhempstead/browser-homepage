d8fd4609b3a50f712b80ecf8a6c0a4874388d693
Matty <matty.hempstead@gmail.com>
2019-10-29 16:27:43 +1100

    Add shortcut elements

--- src/js/shortcuts.js
+++ src/js/shortcuts.js
@@ -3,16 +3,29 @@
  */
 
 const shortcuts = [
-  'https://www.facebook.com/messages/',
-  'https://www.youtube.com/?gl=US',
-  'https://canvas.sydney.edu.au/',
-  'https://edstem.org/dashboard'
+  {
+    url: 'https://www.facebook.com/messages/',
+    name: 'Messenger',
+    img: ''
+  },{
+    url: 'https://www.youtube.com/?gl=US',
+    name: 'Youtube',
+    img: ''
+  },{
+    url: 'https://canvas.sydney.edu.au/',
+    name: 'Canvas',
+    img: ''
+  },{
+    url: 'https://edstem.org/dashboard',
+    name: 'Edstem',
+    img: ''
+  }
 ]
 
 chrome.commands.onCommand.addListener((cmd) => {
   const hotkeyNum = parseInt(cmd.split('-')[1])
   if (hotkeyNum <= shortcuts.length) {
-    const url = shortcuts[hotkeyNum - 1]
+    const url = shortcuts[hotkeyNum - 1].url
     window.open(url, '_top')  
   }
 })
@@ -20,6 +33,26 @@ chrome.commands.onCommand.addListener((cmd) => {
 
 
 // Add each url to page and display Shortcut beneath it
-chrome.commands.getAll((evt) => {
-  console.log('command', evt)
+const hotkeysContainer = document.getElementById('url-hotkeys')
+chrome.commands.getAll((cmds) => {
+  for (cmd of cmds) {
+    const shortcutIndex = parseInt(cmd.shortcut.split('+')[1]) - 1
+
+    // Skip shortcut if url does not exist
+    if (shortcutIndex >= shortcuts.length) break;
+
+    const hotkeySpan = document.createElement('span')
+    hotkeysContainer.appendChild(hotkeySpan)
+
+    // Add name element
+    const nameText = document.createElement('span')
+    hotkeySpan.appendChild(nameText)
+    nameText.innerHTML = shortcuts[shortcutIndex].name
+
+    // Add hotkey element
+    const hotkeyText = document.createElement('span')
+    hotkeySpan.appendChild(hotkeyText)
+    hotkeyText.innerHTML = cmd.shortcut
+
+  }
 })
